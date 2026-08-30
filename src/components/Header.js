9cb8974142e4bb41580a5f317f2db8d886c13b0b
marwamasrour <masrourmarwa06@gmail.com>
2026-08-30 15:05:18 +0100

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="header container">
+      <img
+        src="/logo192.png"
+        alt="Little Lemon logo"
+        className="header__logo"
+      />
+    </header>
+  );
+}
+
+export default Header;
